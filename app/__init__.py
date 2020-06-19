d68246c720c220e9f6116eec444b597f4e379bb6
Tashi Topgyal <ttopgyal@Tserings-MacBook-Pro.local>
2020-06-19 18:11:42 -0400

    merge conflict

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 from flask import Flask, jsonify, json, request, abort, render_template
-=======
-from flask import Flask, render_template, jsonify, json
->>>>>>> d230cd9be154fd1dc54d6e5e529ca7f2c0c66f8c
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 import os
@@ -59,10 +55,4 @@ def index():
 
     except Exception as ex:
         print(ex)
-        
-<<<<<<< HEAD
-    return 'result' 
- 
-=======
-    return 'result' 
->>>>>>> d230cd9be154fd1dc54d6e5e529ca7f2c0c66f8c
+        return 'result' 
