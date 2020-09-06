2396c8aefe8a28d2cf5424f057b2171fc0f0d0b7
Tashi Topgyal <ttopgyal@Tserings-MacBook-Pro.local>
2020-09-06 12:56:11 -0400

    updating databse connection

--- app/__init__.py
+++ app/__init__.py
@@ -2,20 +2,30 @@ from flask import Flask, jsonify, json, request, abort, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 from .models import Amenity
-from .database import engine, Session, Base
+from .database import engine, Base, session_scope
 
 app = Flask(__name__) 
 
-@app.route('/', methods=['GET'])
-def index():
+@app.route('/one', methods=['GET'])
+def one():
     try:
-        session = Session()
-
-        Base.metadata.create_all(bind=engine)
-
-        amenities = session.query(Amenity).filter(Amenity.name == "Gym")
+        with session_scope() as session:
+            amenities = session.query(Amenity).filter(Amenity.name == "Laundry")
         
-        return render_template('index.html', result=amenities)
+            return render_template('index.html', result=amenities)
+
+    except Exception as ex:
+        print(ex)
+        
+    return 'result' 
+
+@app.route('/two', methods=['GET'])
+def two():
+    try:
+        with session_scope() as session:
+            amenities = session.query(Amenity).filter(Amenity.name == "Gym")
+        
+            return render_template('index.html', result=amenities)
 
     except Exception as ex:
         print(ex)
--- app/database.py
+++ app/database.py
@@ -1,10 +1,29 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
+from contextlib import contextmanager
 from .config import Config
 
-engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI, echo=True)
 
-Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
+
+Base.metadata.create_all(bind=engine)
+
+
+@contextmanager
+def session_scope():
+    """Provide a transactional scope around a series of operations."""
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+        
